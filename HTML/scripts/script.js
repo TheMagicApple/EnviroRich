7101532ded58b270aea72e23b09330d261249fec
Duck <87908967+bcomey@users.noreply.github.com>
2022-02-26 15:54:18 -0800

    Added bag

--- HTML/scripts/script.js
+++ HTML/scripts/script.js
@@ -12,6 +12,7 @@ var scene_objects = [];
 water_bottle = new Model('/bottle/bottle.obj', '/bottle/bottle.mtl');
 bike = new Model('/bike/bike.obj', '/bike/bike.mtl');
 trash = new Model('/trash/trash.obj', '/trash/trash.mtl');
+bag = new Model('/bag/bag.obj', '/bag/bag.mtl');
 
 
 //Set up model loader
@@ -38,7 +39,8 @@ function load(toLoad, pos) {
 }
 load(water_bottle, [0, 0, 0]);
 load(bike, [5, 0, 0]);
-load(trash, [-5, 0, 0])
+load(trash, [-5, 0, 0]);
+load(bag, [0, 2, 0]);
 
 
 
